68ddea5342931cbe32bf95fe1999cbb0a1f8750e
Luis Oliveira <luis.andre@psychometrika.com.br>
2023-09-28 10:35:02 -0300

    feat: added error handling

--- src/routes/marcas.js
+++ src/routes/marcas.js
@@ -2,37 +2,62 @@ import { router } from "../index.js";
 import * as carList from "../utils/carList.js";
 
 export const routesBrands = () => {
-  router.get("/maisModelos", async (req, res) => {
-    const brandMore = await carList.maisModelos();
+  router.get("/maisModelos", async (req, res, next) => {
+    try {
+      const brandMore = await carList.maisModelos();
 
-    return res.status(200).json(brandMore);
+      return res.status(200).json(brandMore);
+    } catch (error) {
+      next(error);
+    }
   });
 
-  router.get("/menosModelos", async (req, res) => {
-    const brandMinus = await carList.menosModelos();
+  router.get("/menosModelos", async (req, res, next) => {
+    try {
+      const brandMinus = await carList.menosModelos();
 
-    return res.status(200).json(brandMinus);
+      return res.status(200).json(brandMinus);
+    } catch (error) {
+      next(error);
+    }
   });
 
-  router.get("/listaMaisModelos/:quantity", async (req, res) => {
-    const { quantity } = req.params;
-    const listBrandsMore = await carList.listaMaisModelos(quantity);
+  router.get("/listaMaisModelos/:quantity", async (req, res, next) => {
+    try {
+      const { quantity } = req.params;
+      const listBrandsMore = await carList.listaMaisModelos(quantity);
 
-    return res.status(200).json(listBrandsMore);
+      return res.status(200).json(listBrandsMore);
+    } catch (error) {
+      next(error);
+    }
   });
 
-  router.get("/listaMenosModelos/:quantity", async (req, res) => {
-    const { quantity } = req.params;
-    const listBrandsMinus = await carList.listaMenosModelos(quantity);
+  router.get("/listaMenosModelos/:quantity", async (req, res, next) => {
+    try {
+      const { quantity } = req.params;
+      const listBrandsMinus = await carList.listaMenosModelos(quantity);
 
-    return res.status(200).json(listBrandsMinus);
+      return res.status(200).json(listBrandsMinus);
+    } catch (error) {
+      next(error);
+    }
   });
 
-  router.post("/listaModelos", async (req, res) => {
-    const { nomeMarca } = req.body;
-    const listModels = await carList.listaModelos(nomeMarca);
+  router.post("/listaModelos", async (req, res, next) => {
+    try {
+      const { nomeMarca } = req.body;
+      const listModels = await carList.listaModelos(nomeMarca);
 
-    return res.status(200).json(listModels);
+      return res.status(200).json(listModels);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.use((error, req, res, next) => {
+    console.log(error);
+    return res.status(400).send({ error: error.message });
   });
 
   return router;
